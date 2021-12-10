403324db8267cde51f3527ea3fa71a60dcfc4dd6
Read Langworthy <rlangworthy@gmail.com>
2021-12-09 20:37:24 -0800

    commit tree fd642e836c9b64ad253b681db2c52d45f33e4ad9

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [ imageURL, SetImageURL ] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then( r => r.json() )
+        .then( (d) => SetImageURL(d.message) )
+    }, [] )
+
+    return(
+        <>
+            
+            
+            { imageURL !== "" ? <img src= {imageURL} alt="A Random Dog" /> : <p>Loading...</p> }
+        </>
+    )
+}
+
+export default App;
